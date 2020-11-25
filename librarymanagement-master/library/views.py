c040759c0f25bf4b9f4664c9c958b3b86cc94979
KallaRukmini7 <kallarukmini7@gmail.com>
2020-11-25 09:36:47 +0530

    Added images

--- librarymanagement-master/library/views.py
+++ librarymanagement-master/library/views.py
@@ -95,8 +95,6 @@ def addbook_view(request):
             return render(request,'library/bookadded.html')
     return render(request,'library/addbook.html',{'form':form})
 
-@login_required(login_url='adminlogin')
-@user_passes_test(is_admin)
 def viewbook_view(request):
     books=models.Book.objects.all()
     return render(request,'library/viewbook.html',{'books':books})
