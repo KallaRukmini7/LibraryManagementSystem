5787fb48857da7eb0ca4776de3bcc49622af712a
KallaRukmini7 <kallarukmini7@gmail.com>
2020-12-05 17:06:12 +0530

    Adding ForgotPassword option

--- librarymanagement-master/librarymanagement/urls.py
+++ librarymanagement-master/librarymanagement/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from library import views
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import LoginView,LogoutView,PasswordResetView,PasswordResetCompleteView,PasswordResetDoneView,PasswordResetConfirmView;
 
 
 
@@ -47,6 +47,14 @@ urlpatterns = [
 
     path('contactus', views.contactus_view),
     path('aboutus', views.aboutus_view),
+
+
+
+
+    path('reset_password/',PasswordResetView.as_view(template_name='registration/password_reset.html'),name="reset_password"),
+    path('reset_password_sent/',PasswordResetDoneView.as_view(template_name='registration/password_reset_sent.html'),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(template_name='registration/password_reset_form.html') ,name="password_reset_confirm"),
+    path('reset_password_completed/', PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),name="password_reset_complete"),
 
 
 ]
